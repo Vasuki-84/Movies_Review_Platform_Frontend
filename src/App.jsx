6baf0b34631ae983fa48bf22abbe9ad6f6606dc9
Vasuki-T <iamvasukithangaraj@gmail.com>
2026-01-02 19:29:51 +0530

    created routes for all pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Home from "./Pages/Home";
+import AddReviews from "./Pages/AddReviews";
+import MovieDetails from "./Pages/MovieDetails";
+import Footer from "./Components/Footer";
+import AdminRegister from "./Pages/AdminRegister";
+import AdminLogin from "./Pages/AdminLogin";
+import UserRegister from "./Pages/UserRegister";
+import UserLogin from "./Pages/UserLogin";
 
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/review" element={<AddReviews />} />
+        <Route path="/movies" element={<MovieDetails />} />
+        <Route path="/adminRegister" element={<AdminRegister />} />
+        <Route path="/adminLogin" element={<AdminLogin />} />
+        <Route path="/userRegister" element={<UserRegister />} />
+        <Route path="/userLogin" element={<UserLogin />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Pages/AddReviews.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AddReviews() {
+  return (
+    <div>AddReviews</div>
+  )
+}
+
+export default AddReviews
